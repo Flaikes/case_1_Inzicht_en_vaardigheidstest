7698e0bd20b7398f2b3d806162dd5a891ef6746c
Flaikes <felixdeklerk@gmail.com>
2022-12-15 22:25:29 +0100

    :sparkles: Implemented multi-client server communication and room logic.

--- chat-app/index.js
+++ chat-app/index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
@@ -6,10 +7,177 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-const PORT = 3000 || process.env.PORT;
+app.use(express.static(path.join(__dirname, "public")));
 
-io.on("connection", socket => {
-    console.log("New client")
+// Rooms
+class room{
+    users = [];
+    constructor(id, name){
+        this.id = id;
+        this.name = name;
+    }
+}
+
+var rooms = [];
+addRoom(0, "YouTube");
+addRoom(1, "Netflix");
+addRoom(2, "stack<b>overflow</b>");
+addRoom(3, "Advent of Code");
+
+// Client/server connections
+io.on("connection", socket =>{
+
+    var user = {
+        id: socket.id,
+        name: "",
+        room: -1
+    };
+
+    var privateRooms = [];
+
+    // On login
+    socket.on("join", username_ => {
+        user = userJoin(user.id, username_, -1);
+
+        // To user 
+        socket.emit("messageIn", formatMessage("Mchat*", `<br>Hey, ${user.name}!<br>Select a room to start chatting! &#128512; &#128525; &#128151;<br><br>`));
+        
+        // Show rooms
+        for( let i = 0; i < rooms.length; i++){
+            socket.emit("publicRoom", rooms[i].id, rooms[i].name, rooms[i].users.length)
+        }
+
+        // Show users
+        for( let i = 0; i < users.length; i++){
+            socket.emit("user", users[i]);
+        }
+
+        // Show self to users
+        socket.broadcast.emit("user", user);
+    })
+
+    // On join room
+    var prevRoomIsPrivate = false;
+    socket.on("joinRoom", (room, private) => {
+
+        // Leave previous room
+        if(user.room != -1){
+            // To room that was left
+            socket.broadcast.to(user.room).emit("messageNeutral", `${user.name} left chat`);
+
+            if(!prevRoomIsPrivate){
+                socket.leave(user.room);
+
+                // Update everyone's userlist
+                io.emit("removeUser", user);
+
+                // Remove user from rooms[x]
+                const oldRoomIndex = rooms.findIndex( r => r.id === user.room);
+                rooms[oldRoomIndex].users = rooms[oldRoomIndex].users.filter( u => u.id !== user.id);
+                // Update count
+                io.emit("newCount", user.room, rooms[oldRoomIndex].users.length);
+            }
+        }
+
+        if(private){
+            prevRoomIsPrivate = true;
+        }else{
+            prevRoomIsPrivate = false;
+        }
+
+        // Join new room
+        user.room = room;
+        socket.join(user.room);
+        if(!private){
+            rooms.find( r => r.id === room).users.push(user);
+            io.emit("newCount", user.room, rooms.find( r => r.id === room).users.length);
+
+            // Show users
+            const tempUsers = rooms.find( r => r.id === room).users;
+            for( let i = 0; i < tempUsers.length; i++){
+                socket.emit("user", tempUsers[i]);
+            }
+
+            // Show to users
+            socket.broadcast.emit("user", user);
+        }
+
+        // To everyone but that user
+        socket.broadcast.to(user.room).emit("messageNeutral", `${user.name} joined chat`); 
+        
+        // To the user
+        socket.emit("messageNeutral", `Welcome ${user.name}!`);
+    });
+
+    // On chatmessage
+    socket.on("chatMessage", message => {
+        socket.broadcast.to(user.room).emit("messageIn", formatMessage(user.name, message));
+    });
+
+    // On private message
+    socket.on("privateMessage", (id, name) => {
+        if(privateRooms.includes(`${id}&${user.id}`) == false
+           && privateRooms.includes(`${user.id}&${id}`) == false
+           && id != user.id){ // If not already chatting 
+            socket.emit("privateRoom", `${id}&${user.id}`, name);
+            io.to(id).emit("privateRoom", `${id}&${user.id}`, user.name);
+            privateRooms.push(`${id}&${user.id}`);
+            console.log(id, name);
+        }
+    });
+
+    // On user disconnect, to everyone
+    socket.on("disconnect", () => {
+        io.to(user.room).emit("messageNeutral", `${user.name} left chat`);
+
+        io.emit("removeUser", user);
+    
+        // Leave last room
+        if(user.room != -1){
+            // To room that was left
+            socket.broadcast.to(user.room).emit("messageNeutral", `${user.name} left chat`);
+
+            socket.leave(user.room);
+
+            // Remove user from rooms[x]
+            if(!prevRoomIsPrivate){
+                const oldRoomIndex = rooms.findIndex( r => r.id === user.room);
+                rooms[oldRoomIndex].users = rooms[oldRoomIndex].users.filter( u => u.id !== user.id);
+            }
+        }
+        users = users.filter( u => u.id !== user.id);
+    });
 })
 
-server.listen(PORT, () => console.log("Server is running on port", PORT));
+const PORT = 3000 || process.env.PORT;
+
+let date = new Date();
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+function formatMessage(username, text){
+    return{
+        username,
+        text,
+        time: ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2)
+    } 
+}
+
+let users = [];
+
+function userJoin(id, name, room){
+    const user = {id, name, room};
+    console.log("NEW USER", user);
+    users.push(user);
+
+    return user;
+}
+
+function getUser(id){
+    return users.find(user => user.id === id);
+}
+
+function addRoom(id, name){
+    rooms.push(new room(id, name));
+}
--- chat-app/public/script.js
+++ chat-app/public/script.js
@@ -1,35 +1,175 @@
 const socket = io();
+const date = new Date();
 
-socket.on("message", message => {
-    console.log(message);
-})
-
-// main resize on arrival of sidebar
-const sideBar = document.getElementById("side-bar")
-const main = document.getElementById("main")
-
-function wideMain(){
-    sideBar.classList.remove("hidden");
-    main.classList.remove("wide");
-}
-
-// multi line msg iput
-var msg = "";
-const msgInput = document.getElementById("msgContainer");
-document.getElementById("concept").addEventListener("keypress", submitOnEnter);
-
-// 
-document.getElementById("concept").addEventListener("input", () => {
-    msgInput.setAttribute("data-value",event.target.value + ".");
+// Get rooms
+socket.on("publicRoom", (id, name, count) => {
+    const room = document.createElement("room");
+    room.setAttribute("onclick", `joinRoom(${id})`);
+    room.setAttribute("id", `room${id}`);
+    room.innerHTML = `
+    <img src="https://picsum.photos/20${id}">
+    <container>
+        <name>${name}</name>
+        <sub-title id="sub${id}" count="${count}"> people online</sub-title>
+    </container>
+    `;
+    leftBar.appendChild(room);
 });
 
+// Join room
+var currentRoom = -1;
+
+function joinRoom(room){
+    if(room != currentRoom){
+        //clear chat
+        chat.innerHTML = "";
+        rightBar.innerHTML = "";
+        rightBar.classList.remove("hidden");
+        main.classList.remove("halfWide");
+
+        //communicate room change to server
+        socket.emit("joinRoom", room, false);
+        
+        //update style
+        if(currentRoom != -1){
+            document.getElementById(`room${currentRoom}`).classList.remove("active");
+        }
+        document.getElementById(`room${room}`).classList.add("active");
+        currentRoom = room;
+    }
+}
+
+// Get users
+socket.on("user", (user) => {
+    if(user.room ==  currentRoom){
+        const userElement = document.createElement("user");
+        userElement.setAttribute("onclick", `chatTo("${user.id}", "${user.name}")`);
+        userElement.setAttribute("id", `${user.id}`);
+        userElement.classList.add("in");
+        userElement.innerHTML = `
+        <name>${user.name}</name>
+        `;
+        rightBar.appendChild(userElement);
+    }
+});
+
+// Remove users
+socket.on("removeUser", (user) =>{
+    if(currentRoom != -1){
+        if(user.room == currentRoom){
+            rightBar.removeChild(document.getElementById(user.id));
+        }
+    }
+});
+
+// New private chat
+socket.on("privateRoom", (id, name) => {
+    const room = document.createElement("room");
+    room.setAttribute("onclick", `joinPrivate("${id}")`);
+    room.setAttribute("id", `room${id}`);
+    room.classList.add("private");
+    room.classList.add("new");
+    room.innerHTML = `
+    <img src="https://picsum.photos/209">
+    <container>
+        <name>${name}</name>
+    </container>
+    `;
+    leftBar.appendChild(room);
+});
+
+function chatTo(id, name){
+    socket.emit("privateMessage", id, name);
+}
+
+function joinPrivate(room){
+    if(room != currentRoom){
+        //clear chat
+        chat.innerHTML = "";
+        rightBar.classList.add("hidden");
+        main.classList.add("halfWide");
+
+        //communicate room change to server
+        socket.emit("joinRoom", room, true);
+        
+        //update style
+
+        if(currentRoom != -1){
+            document.getElementById(`room${currentRoom}`).classList.remove("active");
+        }
+        document.getElementById(`room${room}`).classList.add("active");
+        document.getElementById(`room${room}`).classList.remove("new");
+        currentRoom = room;
+    }
+}
+
+// Update user count
+socket.on("newCount", (id, count) => {
+    console.log("newCount", count, id);
+    document.getElementById(`sub${id}`).setAttribute("count", count);
+});
+
+var username = "";
+// Login submit
+document.getElementById("login-form").addEventListener("submit", (event) => {
+    event.preventDefault(); // prevent reload
+    login();
+    username = document.getElementById("username").value;
+    socket.emit("join", username);
+});
+
+// Message IN/OUT/NEUTRAL
+const chat = document.getElementById("chat");
+
+// Message IN
+socket.on("messageIn", message => {
+    addMessageIn(message);
+})
+
+function addMessageIn(message){
+    const bubble = document.createElement("bubble");
+    bubble.classList.add("in");
+    bubble.innerHTML = `
+    <name>${message.username}</name>
+    <p>
+        ${message.text}
+    </p>
+    <time>${message.time}</time>
+    `;
+    chat.appendChild(bubble);
+    chat.scrollTop = chat.scrollHeight;
+}
+
+// Message NEUTRAL
+socket.on("messageNeutral", message => {
+    console.log(message);
+    addMessageNeutral(message);
+})
+
+function addMessageNeutral(message){
+    const bubble = document.createElement("bubble");
+    bubble.classList.add("neutral");
+    bubble.innerHTML = `
+    <p>
+        ${message}
+    </p>
+    `;
+    chat.appendChild(bubble);
+    chat.scrollTop = chat.scrollHeight;
+}
+
+// Message OUT
+
 // Submit message on enter
+var msg = "";
+document.getElementById("concept").addEventListener("keypress", submitOnEnter);
 function submitOnEnter(event){
     if(event.which === 13){ // if enter
         if(!event.shiftKey){ // shift + enter reserved for newline
             msg = event.target.value.trim();
             event.target.value = "";
             event.target.form.dispatchEvent(new Event("submit", {cancelable: true})); // create submit event, catchable by eventlistener
+            event.target.dispatchEvent(new Event("input")); //trigger resize
             event.preventDefault(); // prevent newline
         }
     }
@@ -39,6 +179,45 @@ function submitOnEnter(event){
 document.getElementById("chat-form").addEventListener("submit", (event) => {
     event.preventDefault(); // prevent reload
     if(msg != ""){
+        addMessageOut(msg);
         socket.emit("chatMessage", msg);
     }
 });
+
+function addMessageOut(message){
+    const bubble = document.createElement("bubble");
+    bubble.classList.add("out");
+    bubble.innerHTML = `
+    <p>
+        ${message}
+    </p>
+    <time>${("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2)}</time>
+    `;
+    chat.appendChild(bubble);
+    chat.scrollTop = chat.scrollHeight;
+}
+
+
+// Multi line msg iput
+const msgInput = document.getElementById("msgContainer");
+
+// 
+document.getElementById("concept").addEventListener("input", () => {
+    msgInput.setAttribute("data-value",event.target.value + ".");
+});
+
+// main resize on arrival of sidebar
+const leftBar = document.getElementById("left-bar");
+const rightBar = document.getElementById("right-bar");
+const main = document.getElementById("main");
+const logIn = document.getElementById("login");
+const input = document.getElementById("input");
+
+function login(){
+    leftBar.classList.remove("hidden");
+    rightBar.classList.remove("hidden");
+    main.classList.remove("wide");
+    logIn.classList.add("hidden");
+    chat.classList.remove("hidden");
+    input.classList.remove("hidden");
+}
